ab0de5e17362bfdf60f5ad31000d10a5423a9481
Luka Vandervelden <lukc@upyum.com>
2019-03-17 12:33:11 +0100

    Forgotten watcher.js.

---
+++ watcher.js
@@ -0,0 +1,20 @@
+if ('WebSocket' in window) {
+	(() => {
+		var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+		var address = protocol + window.location.host + window.location.pathname + "@WEBSOCKETPATH";
+		var ws = new WebSocket(address);
+		ws.onopen = () => {
+			console.log("Connected to kemal-watcher");
+		};
+		ws.onmessage = (msg) => {
+			if (msg.data == "reload") {
+				window.location.reload();
+			}
+		};
+		ws.onclose = () => {
+			setTimeout(() => {
+				window.location.reload();
+			}, 2000);
+		};
+	})();
+}
